d61b5a440001e4aef9a1eacea1ee61bd48e5ad89
Connor James Smith <cjs.connor.smith@gmail.com>
2020-09-26 14:57:27 -0400

    updates instal script, sets .ssh and .gnupg file permissions after copy

--- install.sh
+++ install.sh
@@ -1,31 +1,32 @@
 #! /usr/bin/bash
-# this script is a work in progress, do not use
+# this script is a work in progress, do not use if you haven't read it over
 # the following lines require internet
-# TODO: add a version of this script that builds a bare version 
-# of thsi scipt so that I can setup a disconnected server with 
-# a laptop that has already been configured 
-
-#powerline
-pip3 install --user powerline-status
 
 #base16shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 #nvim and development tools
-sudo dnf install "Development Tools"
-sudo dnf install cmake
-sudo dnf install node npm
-sudo dnf install go
-sudo dnf install python-devel
-sudo dnf install neovim
-sudo dnf install rsync
-pip3 install --user pynvim
+sudo dnf group install -y "Development Tools"
+sudo dnf install -y cmake \
+ node npm \
+ go \
+ python-devel \
+ neovim \
+ rsync \
+ ninja-build \
+ gnupg2 pinentry-curses pcsc-lite pcsc-lite-libs gnupg2-smime
 
-#plugged
+# neovim python plugin support
+pip3 install --user pynvim \
+
+#powerline (the cool status line)
+pip3 install --user powerline-status
+
+#plugged (nvim plugin manager)
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-#alacritty
+#alacritty (gpu accelerated terminal)
 sudo dnf copr enable -y pschyska/alacritty
 sudo dnf install -y alacritty  
 
@@ -35,13 +36,29 @@ rsync -aAXP ./.config/alacritty ~/.config
 rsync -aAXP ./.config/fontconfig  ~/.config
 rsync -aAXP ./.config/nvim ~/.config
 rsync -aAXP ./.config/powerline ~/.config
+
+# ssh and folder permissions
 rsync -aAXP ./.ssh ~
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/config
+chmod 600 ~/.ssh/id_rsa_yubikey.pub
+
+#gpg and folder permissions 
 rsync -aAXP ./.gnupg ~
+chmod 700 ~/.gnupg
+chmod 600 ~/.gnupg/gpg-agent.conf
+chmod 600 ~/.gnupg/gpg.conf
+chmod 600 ~/.gnupg/scdaemon.conf
+chmod 600 ~/.gnupg/sshcontrol
+
+#tmux  
 rsync -aAXP ./.tmux.conf ~
 rsync -aAXP ./.gitconfig-source ~/.gitconfig
 
+#restore security context
 restorecon -R ~
 
+#import my public key for gpg 
 gpg --import ./csmith.key
 
 #This runs the PlugInstall function and all post install hooks
